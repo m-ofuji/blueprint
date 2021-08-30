4f289bf2019f9e5209677458d64510c8ee66f5a0
Masato Ofuji <m.ofuji3211@gmail.com>
2021-08-31 01:36:35 +0900

    add button

--- src/Pages/MainPage/MainPage.tsx
+++ src/Pages/MainPage/MainPage.tsx
@@ -1,13 +1,47 @@
+import React from 'react';
 import NavBar from './NavBar';
-import { Page, List, ListItem } from 'react-onsenui';
+import { Page, List, ListItem, SpeedDial, Fab, Icon } from 'react-onsenui';
 
-const MainPage = () => (
-  <Page renderToolbar={() => <NavBar title='BluePrint'/>}>
-    <List
-      dataSource={['Row 1', 'Row 2']}
-      renderRow={(row, idx) => (<ListItem key={idx} modifier='longdivider'>{row}</ListItem>)}
-    />
-  </Page>
-);
+class MainPage extends React.Component
+{
+
+  renderNavbar () {
+    return <NavBar title='BluePrint'/>
+  }
+
+  render() {
+    return (
+      <Page renderToolbar={this.renderNavbar}>
+        <List
+            dataSource={['Row 1', 'Row 2']}
+            renderRow={(row, idx) => (<ListItem key={idx} modifier='longdivider'>{row}</ListItem>)}
+          />
+          <SpeedDial position={'bottom right'}>
+          <Fab>
+            <Icon icon='fa-plus' size={26} fixedWidth={false} />
+          </Fab>
+        </SpeedDial>
+      </Page>
+    )
+  }
+}
+
+// const MainPage = () => (
+//   <Page renderToolbar={() => <NavBar title='BluePrint'/>}>
+//     <List
+//       dataSource={['Row 1', 'Row 2']}
+//       renderRow={(row, idx) => (<ListItem key={idx} modifier='longdivider'>{row}</ListItem>)}
+//     />
+//     <SpeedDial onClick={() => {}} position={'bottom right'}>
+//      <Fab>
+//        <Icon icon='fa-plus' size={26} fixedWidth={false} />
+//      </Fab>
+//      {/* <SpeedDialItem onClick={() => console.log('speed A')}> A </SpeedDialItem>
+//      <SpeedDialItem onClick={() => console.log('speed B')}> B </SpeedDialItem>
+//      <SpeedDialItem onClick={() => console.log('speed C')}> C </SpeedDialItem> */}
+//      {/* <SpeedDialItem onClick={() => console.log('speed D')}> D </SpeedDialItem> */}
+//    </SpeedDial>
+//   </Page>
+// );
 
 export default MainPage;
