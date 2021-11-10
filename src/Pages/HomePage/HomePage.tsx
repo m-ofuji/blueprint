e92ca149bbf80a9346cd2f0b42f1460049cdf973
m-ofuji <m.ofuji3211@gmail.com>
2021-11-10 11:08:11 +0900

    add search bar

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -6,6 +6,9 @@ import { TopoCard } from '../../Components/TopoCard';
 
 const HomePage = ({route, navigator}: {route: any, navigator: Navigator}) => {
   const [topos, setTopos] = useState<ITopo[]>([]);
+  const [searchText, setSearchText] = useState<string>('');
+
+  const searchFunc = (x:ITopo) => x.name.indexOf(searchText) > -1;
 
   const openRightPaintPage = () => {
     handlePaintPage(false);
@@ -30,21 +33,34 @@ const HomePage = ({route, navigator}: {route: any, navigator: Navigator}) => {
 
   useLayoutEffect(updateTopos,[]);
 
+  const onSearchTextChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setSearchText(e.currentTarget.value);
+  }
+
+  const onClrearClicked = (e: React.MouseEvent<HTMLElement>) => {
+    setSearchText('');
+  }
+
   return (
     <Page key={'root'}>
       <div className='search-container'>
-        <input type='text'/>
-        <button>
+        <div>
           <i className={'fas fa-search'}/>
+        </div>
+        <input type='text' value={searchText} onChange={onSearchTextChange}/>
+        <button className={searchText.length > 0 ? 'search-clear' : 'search-clear hidden'} onClick={onClrearClicked}>
+          <i className={'far fa-times-circle'}/>
         </button>
       </div>
 
       <div className={'topo-list'}>
-        {topos.length > 0 ? 
+        {
+          topos.filter(x => searchFunc(x)).length > 0 ?
           topos
+            .filter(x => searchFunc(x))
             .sort((a, b) => a.createdAt > b.createdAt ? -1 : 1)
             .map((x, i) => <TopoCard key={i} {...x} updateTopos={updateTopos}/>)
-          : <p> トポが作成されていません。 </p>
+          : <p> トポが見つかりませんでした </p>
         }
       </div>
       <button className={'edit-button'} onClick={openRightPaintPage}>
