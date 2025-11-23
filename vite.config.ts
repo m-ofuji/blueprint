ae3ac8798c105d14422a4afc6ed918b56e70d538
m-ofuji <m.ofuji3211@gmail.com>
2025-11-23 12:56:18 +0900

    add brotli compression

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import { compression } from 'vite-plugin-compression2'
 import react from '@vitejs/plugin-react'
 import * as path from "path";
 
@@ -7,7 +8,11 @@ export default defineConfig({
     open: true
   },
   plugins: [
-    react()
+    react(),
+    compression({
+      threshold: 1024,
+      algorithms: ['brotliCompress']
+    }),
   ],
   resolve: {
     alias: {
